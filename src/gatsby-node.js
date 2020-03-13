3c30237f82e70ecd19d796f7b4cd7717bbd17565
forgettingpasswords <toms.ozols5@gmail.com>
2020-03-13 15:20:44 +0100

    Compatibility fix to work with scoped npm packages

--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -1,44 +1,44 @@
-import path from "path"
-import { mapValues, isPlainObject, trim } from "lodash"
-import webpack from "webpack"
-import HtmlWebpackPlugin from "html-webpack-plugin"
-import HtmlWebpackExcludeAssetsPlugin from "html-webpack-exclude-assets-plugin"
-import MiniCssExtractPlugin from "mini-css-extract-plugin"
+import path from 'path';
+import { mapValues, isPlainObject, trim } from 'lodash';
+import webpack from 'webpack';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import HtmlWebpackExcludeAssetsPlugin from 'html-webpack-exclude-assets-plugin';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 // TODO: swap back when https://github.com/geowarin/friendly-errors-webpack-plugin/pull/86 lands
-import FriendlyErrorsPlugin from "@pieh/friendly-errors-webpack-plugin"
-import CopyPlugin from "copy-webpack-plugin"
-import HtmlWebpackTagsPlugin from "html-webpack-tags-plugin"
+import FriendlyErrorsPlugin from '@pieh/friendly-errors-webpack-plugin';
+import CopyPlugin from 'copy-webpack-plugin';
+import HtmlWebpackTagsPlugin from 'html-webpack-tags-plugin';
 
 // Deep mapping function for plain objects and arrays. Allows any value,
 // including an object or array, to be transformed.
 function deepMap(obj, fn) {
   // If the transform function transforms the value, regardless of type, return
   // the transformed value.
-  const mapped = fn(obj)
+  const mapped = fn(obj);
   if (mapped !== obj) {
-    return mapped
+    return mapped;
   }
 
   // Recursively deep map arrays and plain objects, otherwise return the value.
   if (Array.isArray(obj)) {
-    return obj.map(value => deepMap(value, fn))
+    return obj.map(value => deepMap(value, fn));
   }
   if (isPlainObject(obj)) {
-    return mapValues(obj, value => deepMap(value, fn))
+    return mapValues(obj, value => deepMap(value, fn));
   }
-  return obj
+  return obj;
 }
 
-const cssTests = []
+const cssTests = [];
 
 function isCssRule({ test }) {
-  return test instanceof RegExp && cssTests.includes(test.toString())
+  return test instanceof RegExp && cssTests.includes(test.toString());
 }
 
 function replaceRule(value, stage) {
   // If `value` does not have a `test` property, it isn't a rule object.
   if (!value || !value.test) {
-    return value
+    return value;
   }
 
   // remove dependency rule
@@ -48,11 +48,9 @@ function replaceRule(value, stage) {
     value.use[0] &&
     value.use[0].options &&
     value.use[0].options.presets &&
-    /babel-preset-gatsby[/\\]dependencies\.js/.test(
-      value.use[0].options.presets
-    )
+    /babel-preset-gatsby[/\\]dependencies\.js/.test(value.use[0].options.presets)
   ) {
-    return null
+    return null;
   }
 
   // use MiniCssExtractPlugin.loader in development
@@ -63,55 +61,52 @@ function replaceRule(value, stage) {
           ...rule,
           loader: MiniCssExtractPlugin.loader,
           options: {
-            hmr: true,
-          },
-        }
+            hmr: true
+          }
+        };
       }
 
-      return rule
+      return rule;
     }
 
     if (value.use) {
       return {
         ...value,
-        use: value.use.map(replaceStyleLoader),
-      }
+        use: value.use.map(replaceStyleLoader)
+      };
     } else if (value.loader) {
       return {
         ...value,
-        loader: replaceStyleLoader(value),
-      }
+        loader: replaceStyleLoader(value)
+      };
     }
   }
 
-  return value
+  return value;
 }
 
 exports.onPreInit = ({ reporter }) => {
   try {
-    require.resolve(`netlify-cms`)
+    require.resolve(`netlify-cms`);
     reporter.warn(
       `The netlify-cms package is deprecated, please install netlify-cms-app instead. You can do this by running "npm install netlify-cms-app"`
-    )
+    );
   } catch (err) {
     // carry on
   }
-}
+};
 
 exports.onCreateDevServer = ({ app, store }, { publicPath = `admin` }) => {
-  const { program } = store.getState()
-  const publicPathClean = trim(publicPath, `/`)
+  const { program } = store.getState();
+  const publicPathClean = trim(publicPath, `/`);
   app.get(`/${publicPathClean}`, function(req, res) {
-    res.sendFile(
-      path.join(program.directory, `public`, publicPathClean, `index.html`),
-      err => {
-        if (err) {
-          res.status(500).end(err.message)
-        }
+    res.sendFile(path.join(program.directory, `public`, publicPathClean, `index.html`), err => {
+      if (err) {
+        res.status(500).end(err.message);
       }
-    )
-  })
-}
+    });
+  });
+};
 
 exports.onCreateWebpackConfig = (
   { store, stage, getConfig, plugins, pathPrefix, loaders, rules, actions },
@@ -123,11 +118,11 @@ exports.onCreateWebpackConfig = (
     htmlTitle = `Content Manager`,
     htmlFavicon = ``,
     manualInit = false,
-    includeRobots = false,
+    includeRobots = false
   }
 ) => {
   if (![`develop`, `build-javascript`].includes(stage)) {
-    return Promise.resolve()
+    return Promise.resolve();
   }
 
   // populate cssTests array later used by isCssRule
@@ -139,36 +134,39 @@ exports.onCreateWebpackConfig = (
         /\.s(a|c)ss$/,
         /\.module\.s(a|c)ss$/,
         /\.less$/,
-        /\.module\.less$/,
+        /\.module\.less$/
       ].map(t => t.toString())
-    )
+    );
   }
 
-  const gatsbyConfig = getConfig()
-  const { program } = store.getState()
-  const publicPathClean = trim(publicPath, `/`)
+  const gatsbyConfig = getConfig();
+  const { program } = store.getState();
+  const publicPathClean = trim(publicPath, `/`);
 
   const externals = [
     {
       name: `react`,
       global: `React`,
       assetDir: `umd`,
-      assetName: `react.production.min.js`,
+      assetName: `react.production.min.js`
     },
     {
       name: `react-dom`,
       global: `ReactDOM`,
       assetDir: `umd`,
-      assetName: `react-dom.production.min.js`,
-    },
-    {
+      assetName: `react-dom.production.min.js`
+    }
+  ];
+
+  if (!manualInit) {
+    externals.unshift({
       name: `netlify-cms-app`,
       global: `NetlifyCmsApp`,
       assetDir: `dist`,
       assetName: `netlify-cms-app.js`,
-      sourceMap: `netlify-cms-app.js.map`,
-    },
-  ]
+      sourceMap: `netlify-cms-app.js.map`
+    });
+  }
 
   if (enableIdentityWidget) {
     externals.unshift({
@@ -176,27 +174,22 @@ exports.onCreateWebpackConfig = (
       global: `netlifyIdentity`,
       assetDir: `build`,
       assetName: `netlify-identity-widget.js`,
-      sourceMap: `netlify-identity-widget.js.map`,
-    })
+      sourceMap: `netlify-identity-widget.js.map`
+    });
   }
 
   const config = {
     ...gatsbyConfig,
     entry: {
-      cms: [
-        path.join(__dirname, `cms.js`),
-        enableIdentityWidget && path.join(__dirname, `cms-identity.js`),
-      ]
+      cms: [path.join(__dirname, `cms.js`), enableIdentityWidget && path.join(__dirname, `cms-identity.js`)]
         .concat(modulePath)
-        .filter(p => p),
+        .filter(p => p)
     },
     output: {
-      path: path.join(program.directory, `public`, publicPathClean),
+      path: path.join(program.directory, `public`, publicPathClean)
     },
     module: {
-      rules: deepMap(gatsbyConfig.module.rules, value =>
-        replaceRule(value, stage)
-      ).filter(Boolean),
+      rules: deepMap(gatsbyConfig.module.rules, value => replaceRule(value, stage)).filter(Boolean)
     },
     plugins: [
       // Remove plugins that either attempt to process the core Netlify CMS
@@ -204,8 +197,7 @@ exports.onCreateWebpackConfig = (
       ...gatsbyConfig.plugins.filter(
         plugin =>
           ![`MiniCssExtractPlugin`, `GatsbyWebpackStatsExtractor`].find(
-            pluginName =>
-              plugin.constructor && plugin.constructor.name === pluginName
+            pluginName => plugin.constructor && plugin.constructor.name === pluginName
           )
       ),
 
@@ -217,17 +209,17 @@ exports.onCreateWebpackConfig = (
           clearConsole: false,
           compilationSuccessInfo: {
             messages: [
-              `Netlify CMS is running at ${program.ssl ? `https` : `http`}://${
-                program.host
-              }:${program.port}/${publicPathClean}/`,
-            ],
-          },
+              `Netlify CMS is running at ${program.ssl ? `https` : `http`}://${program.host}:${
+                program.port
+              }/${publicPathClean}/`
+            ]
+          }
         }),
 
       // Use a simple filename with no hash so we can access from source by
       // path.
       new MiniCssExtractPlugin({
-        filename: `[name].css`,
+        filename: `[name].css`
       }),
 
       // Auto generate CMS index.html page.
@@ -237,8 +229,8 @@ exports.onCreateWebpackConfig = (
         chunks: [`cms`],
         excludeAssets: [/cms.css/],
         meta: {
-          robots: includeRobots ? `all` : `none`, // Control whether search engines index this page
-        },
+          robots: includeRobots ? `all` : `none` // Control whether search engines index this page
+        }
       }),
 
       // Exclude CSS from index.html, as any imported styles are assumed to be
@@ -249,7 +241,7 @@ exports.onCreateWebpackConfig = (
       // Pass in needed Gatsby config values.
       new webpack.DefinePlugin({
         __PATH__PREFIX__: pathPrefix,
-        CMS_PUBLIC_PATH: JSON.stringify(publicPath),
+        CMS_PUBLIC_PATH: JSON.stringify(publicPath)
       }),
 
       new CopyPlugin(
@@ -259,12 +251,12 @@ exports.onCreateWebpackConfig = (
             [
               {
                 from: require.resolve(path.join(name, assetDir, assetName)),
-                to: assetName,
+                to: assetName
               },
               sourceMap && {
                 from: require.resolve(path.join(name, assetDir, sourceMap)),
-                to: sourceMap,
-              },
+                to: sourceMap
+              }
             ].filter(item => item)
           )
         )
@@ -272,13 +264,13 @@ exports.onCreateWebpackConfig = (
 
       new HtmlWebpackTagsPlugin({
         tags: externals.map(({ assetName }) => assetName),
-        append: false,
+        append: false
       }),
 
       new webpack.DefinePlugin({
         CMS_MANUAL_INIT: JSON.stringify(manualInit),
-        PRODUCTION: JSON.stringify(stage !== `develop`),
-      }),
+        PRODUCTION: JSON.stringify(stage !== `develop`)
+      })
     ].filter(p => p),
 
     // Remove common chunks style optimizations from Gatsby's default
@@ -287,15 +279,15 @@ exports.onCreateWebpackConfig = (
     optimization: {
       // Without this, node can get out of memory errors when building for
       // production.
-      minimizer: stage === `develop` ? [] : gatsbyConfig.optimization.minimizer,
+      minimizer: stage === `develop` ? [] : gatsbyConfig.optimization.minimizer
     },
     devtool: stage === `develop` ? `cheap-module-source-map` : `source-map`,
     externals: externals.map(({ name, global }) => {
       return {
-        [name]: global,
-      }
-    }),
-  }
+        [name]: global
+      };
+    })
+  };
 
   if (customizeWebpackConfig) {
     customizeWebpackConfig(config, {
@@ -305,8 +297,8 @@ exports.onCreateWebpackConfig = (
       getConfig,
       rules,
       loaders,
-      plugins,
-    })
+      plugins
+    });
   }
 
   actions.setWebpackConfig({
@@ -317,37 +309,37 @@ exports.onCreateWebpackConfig = (
         : {
             splitChunks: {
               cacheGroups: {
-                "netlify-identity-widget": {
+                'netlify-identity-widget': {
                   test: /[\\/]node_modules[\\/](netlify-identity-widget)[\\/]/,
                   name: `netlify-identity-widget`,
                   chunks: `all`,
-                  enforce: true,
-                },
-              },
-            },
+                  enforce: true
+                }
+              }
+            }
           },
     // ignore netlify-identity-widget when not enabled
     plugins: enableIdentityWidget
       ? []
       : [
           new webpack.IgnorePlugin({
-            resourceRegExp: /^netlify-identity-widget$/,
-          }),
-        ],
-  })
+            resourceRegExp: /^netlify-identity-widget$/
+          })
+        ]
+  });
 
   return new Promise((resolve, reject) => {
     if (stage === `develop`) {
-      webpack(config).watch({}, () => {})
+      webpack(config).watch({}, () => {});
 
-      return resolve()
+      return resolve();
     }
 
     return webpack(config).run((err, stats) => {
-      if (err) return reject(err)
-      const errors = stats.compilation.errors || []
-      if (errors.length > 0) return reject(stats.compilation.errors)
-      return resolve()
-    })
-  })
-}
+      if (err) return reject(err);
+      const errors = stats.compilation.errors || [];
+      if (errors.length > 0) return reject(stats.compilation.errors);
+      return resolve();
+    });
+  });
+};
